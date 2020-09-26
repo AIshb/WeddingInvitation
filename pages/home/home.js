11b9971cacb34571d2d218aa5dea1f1fcf934dc4
shihongbin <shihongbin@meituan.com>
2020-09-26 12:56:15 +0800

    邀请函首页

---
+++ pages/home/home.js
@@ -0,0 +1,72 @@
+// pages/home/home.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bgImg: "/images/bg.jpg",
+    editImg: "",
+    inviteName: "石鸿斌&刘梦眙",
+    inviteDateOne: "谨定于 2020年10月24日",
+    inviteDateTwo: "农历 2020年九月初八 举办婚礼",
+    inviteAddress: "地址：昆山皇冠国际会展酒店"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
