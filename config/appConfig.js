cc345e6ac1371ae82aefdd92c0cd81fa744a7819
AIshb <shb_6600@qq.com>
2020-10-10 00:55:55 +0800

    添加分号

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,3 +1,3 @@
 export const CDN_PATH = 'https://3gimg.qq.com/lightmap/xcx/demoCenter/images';
-export const KEY = 'NMXBZ-4RQWU-ONDVL-22542-ZZ2TH-IFFZV'
+export const KEY = 'NMXBZ-4RQWU-ONDVL-22542-ZZ2TH-IFFZV';
 export const REFERER = '石鸿斌和刘梦眙的婚礼邀请';
